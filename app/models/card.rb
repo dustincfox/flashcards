230d1eef2fd9e84d032d7f42871a017350460d95
Dustin Fox <dustincfox@gmail.com>
2014-02-21 13:46:30 -0600

    added validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+
+  validates :deck_id, presence: true
+  validates :question, presence: true
+  validates :answer, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :rounds
   has_many :decks, through: :rounds
+
+  validates :username, length: {maximum: 10}, uniqueness: true, presence: true, format: /\A[ 0-9a-z]+\z/i
+  validates :password, length: {maximum: 12}
 end
