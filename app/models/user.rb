073a31ab0742bc09fc4c9f06271add0f63cb737b
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-21 17:31:16 -0600

    fixed update password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :rounds
   has_many :decks, through: :rounds
   validates :username, length: {maximum: 10}, uniqueness: true, presence: true, format: /\A[ 0-9a-z]+\z/i
-  validates :password, length: {maximum: 12}
+  validates :password, length: {maximum: 12}, presence: true
   before_save :encrypt
 
   #this is going to be our method for defining
