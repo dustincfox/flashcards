654ed9f48092a6a2bc385e04137e88d3a1d5b049
Apprentice <apprentice@dbc-chicago-016.local>
2014-02-21 13:11:41 -0600

    just resyncing

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,15 +1,11 @@
-require 'digest/md5'
-
-
-
 get '/login' do
   erb :login_screen
 end
 
 post '/login' do
   if User.authenticate(params[:username],params[:password])
-    puts "passed Welcome Commander"
+    erb :_login_responce, :locals => {message: "Passed: Welcome Commander"}
   else
-    puts "you done fucked up"
+    erb :_login_responce, :locals => {message: "Failed: you done fucked up"}
   end
 end
