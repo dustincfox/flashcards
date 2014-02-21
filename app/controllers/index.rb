0f3dd7a816be6928aae92ede48f2bbf367cee63f
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-21 10:09:52 -0600

    initial commit; skeleton file

---
+++ app/controllers/index.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
+  @block = "#{params}"
+  erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @input = params
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  @post = params
+  erb :post_cool_url
+end
