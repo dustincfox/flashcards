aae517761d79c74637e4e7a43e4be49a2535ac03
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-21 14:45:01 -0600

    added like a thousand routes and some walrus stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,27 +13,41 @@ post '/logout' do
 
 end
 
+
 get '/round/new/?' do
   @decks = Deck.all
   erb :new_round
 end
 
+post '/round/create' do
+  @deck = Deck.find(params[:deck_id].to_i)
+  @deck_id = @deck.id
+  Round.create(deck_id: @deck_id, user_id: 1) #session[:user_id])
+  session[:cards] = @deck.cards
+  redirect to("/round/#{session[:cards].shuffle.pop.id}")
+end
+
+# get '/round/:card_id/?' do
+#   @current_card = Card.find(params[:card_id].to_i)
+#   @prompt = @current_card.question
+#   @answer = @current_card.answer
+# end
+
+get '/round/next' do
+  redirect to("/round/#{session[:cards].shuffle.pop.id}")
+end
+
 get '/round/:card_id/?' do
   @current_card = Card.find(params[:card_id].to_i)
-  @prompt = @current_card.question
-  @answer = @current_card.answer
+  erb :question
 end
 
-post '/round/new' do
-
-end
-
-get '/round/question/answer/?' do
-
-end
-
-get '/round/question/prompt/?' do
-
+post '/round/:card_id/outcome' do
+  @guess = params[:answer]
+  @answer = Card.find(params[:card_id].to_i).answer
+  @outcome = "Sorry, the correct answer is #{@answer}."
+  @outcome = "Nice job Walrus" if @answer == @guess
+  erb :result
 end
 
 get '/round/complete/?' do
