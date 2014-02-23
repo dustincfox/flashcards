7d044d0744ec920f84216d95dba3c5539c1bbba1
Cristin O'Connor <cristinjaneo@gmail.com>
2014-02-23 10:45:20 -0600

    initial branch commit

--- public/js/application.js
+++ public/js/application.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     $('#question-form').on('submit', function(event) {
         event.preventDefault();
         var data = ($("#answer").serialize())
-        console.log(data)
+        //console.log(data)
         var path = window.location.pathname;
-        console.log(path)
+        //console.log(path)
         var route = (path + "/outcome")
-        console.log
+        //console.log
         $.post(route, data, function(response) {
-            console.log("reponse:" + response)
+            //console.log("reponse:" + response)
             $("#result_container").html(response);
         });
     });
